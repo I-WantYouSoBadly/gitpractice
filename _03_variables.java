ed2efadffcee8d6011ea0b4830db23c75e9ce19b
I-WantYouSoBadly <jh040912@naver.com>
2023-07-12 01:13:49 +0900

    New Files

---
+++ _03_variables.java
@@ -0,0 +1,11 @@
+package Chapter01.git-Java-01;
+
+public class _03_variables {
+    public static void main(String[] args) {
+        String name = "코틀린";
+        int hour = 15;
+
+        System.out.println(name+ " 배송이 시작됩니다. 15시에 방문 예정입니다.");
+        System.out.println(name+ " 배송이 완료되었습니다.");
+    }
+}
