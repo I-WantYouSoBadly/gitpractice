1b64fbb77f0c5592281881ac89bd5322fa4c5213
Znokir <jh040912@naver.com>
2023-07-16 02:38:12 +0900

    230716
    git commit -m

---
+++ _11_WhileLoop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class _11_WhileLoop {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+
+        while (name.isBlank()) {
+            System.out.println("Enter your name: ");
+            name = scanner.nextLine();
+
+        }
+        System.out.println("Hello "+name);
+    }
+}
