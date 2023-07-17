0b72810ec6874c573727e8a11c1099f910b7396a
Znokir <139128705+I-WantYouSoBadly@users.noreply.github.com>
2023-07-18 02:21:14 +0900

    fixed error for _14_Arrays.java

--- _14_Arrays.java
+++ _14_Arrays.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class _14_Arrays {
+public class _14_Arrays {
+    public static void main(String[] args) {
+        // array = used to store multiple values in a single variable
+        String[] cars = {"Camaro", "Corvette", "Tesla", "BMW"};
+        cars[0] = "Mustang";
+        for (int i=0;i<cars.length;i++){
+            System.out.println(cars[i]);
+        }
+    }
 }
