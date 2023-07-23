184e32f8a05e9ea286fa2b6ab61157715fb42ec8
Znokir <jh040912@naver.com>
2023-07-24 04:58:57 +0900

    230724s

---
+++ _26_car.java
@@ -0,0 +1,16 @@
+public class _26_car {
+
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year = 2020;
+    String color = "blue";
+    double price = 55000.00;
+
+    void drive() {
+        System.out.println("You drive the car");
+    }
+    void brake() {
+        System.out.println("You step on the brake");
+    }
+
+}
