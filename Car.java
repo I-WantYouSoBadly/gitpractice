d480fde6b3278241a31d6a1b5581aa391969f501
Znokir <jh040912@naver.com>
2023-07-28 03:18:15 +0900

    230728s

---
+++ Car.java
@@ -0,0 +1,14 @@
+public class Car{
+        String make = "Ford";
+        String model = "Mustang";
+        String color = "Red";
+        int year = 2021;
+
+        public String toString() {
+            String myString = make + "\n"+model+"\n"+color+"\n"+year;
+            return myString;
+
+        }
+
+    }
+
